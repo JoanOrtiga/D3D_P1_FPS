b99b77015c97d259e1151bc69435ddf8e0fe668d
Marc <marcgaldo.97@gmail.com>
2020-10-23 20:38:47 +0200

    all doors implemented

---
+++ Assets/OpenDoorPoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorPoints : MonoBehaviour
+{
+    public FPS_CharacterController fps;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("points", false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fps.currentPoints >= 15)
+        {
+            anim.SetBool("points", true);
+        }
+        else if (fps.currentPoints < 15)
+        {
+            anim.SetBool("points", false);
+        }
+    }
+}
