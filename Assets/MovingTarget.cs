db8396a3542fcdaeaf2d05c16ec89cf1e9da0449
Marc <marcgaldo.97@gmail.com>
2020-10-24 00:17:47 +0200

    .

---
+++ Assets/MovingTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.CompareTag("MovingTarget"))
+        {
+            anim.Play("Movment");
+        }
+    }
+
+}
