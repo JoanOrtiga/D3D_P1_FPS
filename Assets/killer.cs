6e8fac6c319682d2e9445365aab8129d513b73b3
Marc <marcgaldo.97@gmail.com>
2020-10-24 01:08:57 +0200

    killer Floor

---
+++ Assets/killer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killer : MonoBehaviour
+{
+    public FPS_CharacterController fps;
+    private int damage=50;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            fps.LoseHeal(damage);
+    }
+}
